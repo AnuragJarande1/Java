77cdfcb4dd05b4df675687e4158570d0c242b3af
AnuragJarande1 <anuragjarande1249@gmail.com>
2024-02-16 21:34:48 +0530

    linkedlist

---
+++ Palindromelinkedlist.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head==null && head.next!=null){
+            return true;
+        }
+        ListNode mid=findmid(head);
+        ListNode prev=null;
+        ListNode curr=mid;
+        ListNode next;
+
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        ListNode right=prev;
+        ListNode left=head;
+
+        while(right!=null){
+            if(left.val!=right.val){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+
+      
+    }
+
+    public ListNode findmid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
