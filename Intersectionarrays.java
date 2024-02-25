29ce5ae3f1345d5bd74642900fbf0c39568d1914
AnuragJarande1 <anuragjarande1249@gmail.com>
2024-02-25 23:08:44 +0530

    arrays

---
+++ Intersectionarrays.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int n=nums1.length, m=nums2.length;
+        int i=0;
+        int j=0;
+        List<Integer> list= new ArrayList<>();
+        while(i<n && j<m){
+            int a=nums1[i], b=nums2[j];
+            if(a==b){
+                list.add(a);
+                i++;
+                j++;
+            }else if(a<b){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        int arr[]= new int[list.size()];
+        for(int k=0;k<list.size();k++){
+            arr[k]=list.get(k);
+        }
+        return arr;
+    }
+}
