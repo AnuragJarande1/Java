09e8488422e803a626e6861f7223b46955098a30
AnuragJarande1 <anuragjarande1249@gmail.com>
2024-03-05 21:24:41 +0530

    array

---
+++ mergesortedarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+       int i=m-1;
+       int j=n-1;
+       int k=m+n-1;
+
+       while(j>=0){
+           if(i>=0 && arr1[i]>arr2[j]){
+               arr1[k]=arr1[i];
+               k--;
+               i--;
+           }else{
+               arr1[k]=arr2[j];
+               k--;
+               j--;
+           }
+       }
+        // for(int l=0;l<m+n;l++){
+        //     System.out.print(arr1[l]+" ");
+        // }
+    }
+}
